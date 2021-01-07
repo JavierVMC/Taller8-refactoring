3905bba1879c5fed1b2f8ef015e8d40a5e550fb6
JavierVMC <javavega@espol.edu.ec>
2021-01-07 11:25:12 -0500

    Long parameter list and duplicate code

--- TalllerRefactoring/src/modelos/Estudiante.java
+++ TalllerRefactoring/src/modelos/Estudiante.java
@@ -79,13 +79,11 @@ public class Estudiante{
     }
     
     //Calcula y devuelve la nota inicial contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
-    public double CalcularNotaInicial(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
+    public double CalcularNotaInicial(Notas notas){
         double notaInicial=0;
         for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
-                double notaPractico=(ntalleres)*0.20;
-                notaInicial=notaTeorico+notaPractico;
+            if(notas.getP().equals(par)){
+                notaInicial = notaFinal(notas.getNexamen(),notas.getNdeberes(),notas.getNlecciones(),notas.getNtalleres());
             }
         }
         return notaInicial;
@@ -93,13 +91,11 @@ public class Estudiante{
     
     //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
     
-    public double CalcularNotaFinal(Paralelo p, double nexamen,double ndeberes, double nlecciones, double ntalleres){
+    public double CalcularNotaFinal(Notas notas){
         double notaFinal=0;
         for(Paralelo par:paralelos){
-            if(p.equals(par)){
-                double notaTeorico=(nexamen+ndeberes+nlecciones)*0.80;
-                double notaPractico=(ntalleres)*0.20;
-                notaFinal=notaTeorico+notaPractico;
+            if(notas.getP().equals(par)){
+                notaFinal = notaFinal(notas.getNexamen(),notas.getNdeberes(),notas.getNlecciones(),notas.getNtalleres());
             }
         }
         return notaFinal;
@@ -116,6 +112,11 @@ public class Estudiante{
         }
         return notaTotal;
         
+    }
+    
+    
+    public double notaFinal(double nexamen, double ndeberes, double nlecciones, double ntalleres){
+        return ((nexamen+ndeberes+nlecciones)*0.80) + ((ntalleres)*0.20);
     }
 }
         
